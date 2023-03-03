ff9d1c0212b79eaf065c6189080c02807caa6a68
Ian DC <jokerzamba@hotmail.com>
2023-03-02 20:09:21 -0800

    Move /register and /login from index.js to userController.js

--- functions/controllers/userController.js
+++ functions/controllers/userController.js
@@ -1,4 +1,4 @@
-const { v4: uuidv4 } = require("uuid");
+const admin = require("firebase-admin");
 const {
   getFirestore,
   Timestamp,
@@ -6,28 +6,77 @@ const {
 } = require("firebase-admin/firestore");
 const db = getFirestore();
 
-// Create => POST
-exports.newUser = (req, res) => {
-  const userId = uuidv4();
-  db.collection("user")
-    .doc(`${userId}`)
-    .set({
-      name: req.body.name,
-      position: req.body.position,
-      company: req.body.company,
+const isEmpty = (str) => {
+  if (str === "") return true;
+};
+
+// Authentication: register new user
+// POST: create new user document in 'user' collection
+exports.registerUser = (req, res) => {
+  const password = req.body.password;
+  const confirm_password = req.body.confirm_password;
+  const user = {
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    email: req.body.email,
+    created_at: new Date().toISOString(),
+  };
+
+  let errors = {};
+
+  if (isEmpty(user.first_name))
+    errors.first_name = "First Name field must not be empty";
+  if (isEmpty(user.last_name))
+    errors.last_name = "Last Name field must not be empty";
+  if (password !== confirm_password) errors.password = "Passwords must match";
+
+  if (Object.keys(errors).length > 0)
+    return res.status(400).send({ error: errors });
+
+  admin
+    .auth()
+    .createUser(user)
+    .then((userRecord) => {
+      console.log(userRecord);
+      const uid = userRecord.uid;
+      db.collection("user").doc(`${uid}`).set(user);
+      return uid;
     })
-    .then(() => {
-      console.log(`User ${userId} successfully created`);
-      return res.status(200).send();
+    .then((uid) => {
+      return res.status(201).send({
+        message: `Successfully created new user: ${uid}`,
+      });
     })
-    .catch((err) => {
-      console.log(err);
-      return res.status(500).send(err);
+    .catch((error) => {
+      if (error.code == "auth/email-already-exists") {
+        return res.status(400).send(error);
+      }
+      return res.status(500).send(error);
     });
 };
 
-// Read => GET
-// Single User with ID
+// Authentication: return user custom id token on login
+exports.loginUser = (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  admin
+    .auth()
+    .getUserByEmail(email)
+    .then((userRecord) => {
+      const uid = userRecord.uid;
+      return admin.auth().createCustomToken(uid);
+    })
+    .then((customToken) => {
+      res.send({ customToken });
+    })
+    .catch((error) => {
+      console.error(`Error logging in: ${error}`);
+      res.status(401).send("Invalid email or password");
+    });
+};
+
+// GET: single user with id
 exports.singleUser = (req, res) => {
   db.collection("user")
     .doc(req.params.id)
@@ -45,7 +94,8 @@ exports.singleUser = (req, res) => {
       return res.status(500).send({ error: "Server error" });
     });
 };
-// All users
+
+// GET: all users
 exports.allUsers = async (req, res) => {
   try {
     const snapshot = await db.collection("user").get();
@@ -60,7 +110,7 @@ exports.allUsers = async (req, res) => {
   }
 };
 
-// Update => PATCH
+// PATCH: update single user document with id
 exports.updateUser = (req, res) => {
   const updateObject = req.body;
   db.collection("user")
@@ -84,8 +134,7 @@ exports.updateUser = (req, res) => {
     });
 };
 
-// Delete
-// Single User with ID
+// DELETE: single user with id
 exports.deleteUser = (req, res) => {
   db.collection("user")
     .doc(`${req.params.id}`)
