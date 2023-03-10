88f637fdc60af36f6bf110896d45ec58ecc6a5a1
Ian DC <jokerzamba@hotmail.com>
2023-03-10 13:01:21 -0800

    Fux bug on userSchema.js

--- functions/schemas/userSchema.js
+++ functions/schemas/userSchema.js
@@ -1,23 +1,21 @@
-const { z, string } = require("zod")
+const { z, string } = require("zod");
 
 exports.userLocationSchema = z.object({
-    city: string().trim().optional().nullable(),
-    state_province: string().trim().optional().nullable(),
-    country: string().trim().optional().nullable(),
-})
-exports.baseUserSchema = z.object({
-    first_name: z.string().trim().toLowerCase(),
-    last_name: z.string().trim().toLowerCase(),
-    email: z.string().trim().toLowerCase().email(),
-    profile_picture: string().url().optional().nullable(),
-    profile_banner: z.string().url().optional().nullable(),
-    headline: string().trim().optional().nullable(),
-    linkedin: string().trim().optional().nullable(),
-    location: exports.userLocationSchema.optional().nullable(),
-    about: string().trim().optional().nullable(),
-    created_at: string().trim().optional().nullable()
+  city: string().trim().optional().nullable(),
+  state_province: string().trim().optional().nullable(),
+  country: string().trim().optional().nullable(),
 });
-exports.createUserSchema = baseUserSchema
-exports.updateUserSchema = baseUserSchema
-
-
+exports.baseUserSchema = z.object({
+  first_name: z.string().trim().toLowerCase(),
+  last_name: z.string().trim().toLowerCase(),
+  email: z.string().trim().toLowerCase().email(),
+  profile_picture: string().url().optional().nullable(),
+  profile_banner: z.string().url().optional().nullable(),
+  headline: string().trim().optional().nullable(),
+  linkedin: string().trim().optional().nullable(),
+  location: exports.userLocationSchema.optional().nullable(),
+  about: string().trim().optional().nullable(),
+  created_at: string().trim().optional().nullable(),
+});
+exports.createUserSchema = exports.baseUserSchema;
+exports.updateUserSchema = exports.baseUserSchema;
